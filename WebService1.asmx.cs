467f770f8d0512e6f259c9be7f7b62618e9a59cf
Divalcast <102643624+Divalcast@users.noreply.github.com>
2022-03-29 23:18:25 -0600

    Add files via upload

---
+++ WebService1.asmx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WebApplicationFibonacci
+{
+    /// <summary>
+    /// Summary description for WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public int Fibonacci(int numero)
+        {
+            int f = 0;
+            int t1 = 1;
+            int t2;
+            for (int i = 1; i <= numero; i++)
+            {
+
+                t2 = f;
+                f = t1 + f;
+                t1 = t2;
+
+            }
+            return t1;
+        }
+    }
+}
